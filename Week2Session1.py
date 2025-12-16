3f69e86514127b8cfb9b1c9b8850d0b559e4b7b3
Karan-3406 <karansaha2006@gmail.com>
2025-12-16 10:54:54 +0000

    Add files via upload

---
+++ Week2Session1.py
@@ -0,0 +1,20 @@
+#Activity 1
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print ("Hello", name,"and you are", age ,"years old.")
+
+#Activity 2
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+product = a * b
+print("Sum is",sum)
+print("Product is",product)
+
+#Activity 3
+product_name = input("Enter product name: ")
+price = float(input("Enter price: "))
+quantity = int(input("Enter quantity: "))
+total_cost = price * quantity
+print("You bought",quantity,"of",product_name)
+print("Total cost is",total_cost)
